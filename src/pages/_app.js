039c46b9f3718e855ecdd409cb6cd57ea1936d81
Petya Naydenova Ferreira <petianaidenova@gmail.com>
2023-02-08 12:46:28 +0100

    setting up mocks with msw

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,9 @@
 import '@/styles/globals.css'
 
+import('../mocks').then(({setupMocks}) => {
+  setupMocks();
+})
+
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
